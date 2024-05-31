ed7986075e7e64f1cd0bc6d61bc97f6d98d5d309
Advait Churi <advaitchuri@Advaits-MacBook-Pro.local>
2024-05-30 20:50:51 -0700

    Two_Pointers

--- Three_sum.java
+++ Three_sum.java
@@ -18,7 +18,7 @@ class Solution {
 
         for(int i=0; i<nums.length; i++){
 
-            if(i>1 && nums[i]==nums[i-1]){
+            if(i>0 && nums[i]==nums[i-1]){
                 continue;
             }
             
